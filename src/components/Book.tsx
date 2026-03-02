f4dc44344ea99bd962d103cee0727f244eb24bae
akayda008 <akaashjeyakumar@gmail.com>
2026-03-02 15:13:53 +0530

    Page Height Measure Display

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { Book as BookType } from "@/types/book";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type BookProps = {
   book: BookType;
 };
 
+type Page = {
+  id: string;
+  title: string;
+  fullText: string;
+};
+
 export default function Book({ book }: BookProps) {
-  const pages = paginateChapters(book.chapters);
+  const [pages, setPages] = useState<Page[]>([]);
   const [spreadIndex, setSpreadIndex] = useState(0);
 
   const totalPages = pages.length;
@@ -32,69 +38,144 @@ export default function Book({ book }: BookProps) {
     }
   }
 
-  function paginateChapters(chapters: BookType["chapters"]) {
-    const PAGE_MAX_LIMIT = 800;
+  function paginateByHeight(
+    chapters: BookType["chapters"],
+    measuredHeight: HTMLDivElement,
+  ) {
+    const pages = [{ id: "blank", title: "", fullText: "" }];
 
-    const resultPages = [{ id: "blank", title: "", fullText: "" }];
+    chapters.forEach((chapter) => {
+      const words = chapter.fullText.split(" ");
+      const measure = measuredHeight;
 
-    chapters.forEach((chapters) => {
-      const text = chapters.fullText;
-      let start = 0;
-      let pageNumber = 0;
+      let currentWords: string[] = [];
+      let isFirstPage = true;
+      let pageIndex = 0;
 
-      while (start < text.length) {
-        const slice = text.slice(start, start + PAGE_MAX_LIMIT);
+      for (const word of words) {
+        currentWords.push(word);
+        const candidateText = currentWords.join(" ");
+        measure.innerText = candidateText;
 
-        resultPages.push({
-          id: chapters.id + "--page--" + pageNumber,
-          title: pageNumber === 0 ? chapters.title : "",
-          fullText: slice,
+        if (measure.scrollHeight > measure.clientHeight) {
+          currentWords.pop();
+          pages.push({
+            id: chapter.id + "--page--" + pageIndex,
+            title: isFirstPage ? chapter.title : "",
+            fullText: currentWords.join(" "),
+          });
+          isFirstPage = false;
+          pageIndex++;
+          currentWords = [word];
+        }
+      }
+      if (currentWords.length) {
+        pages.push({
+          id: chapter.id + "--page--" + pageIndex,
+          title: isFirstPage ? chapter.title : "",
+          fullText: currentWords.join(" "),
         });
-        start += PAGE_MAX_LIMIT;
-        pageNumber++;
       }
     });
-    return resultPages;
+    return pages;
   }
+
+  const measureRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (!measureRef.current) return;
+    console.log(
+      "Client Height:",
+      measureRef.current.clientHeight,
+      "Scroll Height:",
+      measureRef.current.scrollHeight,
+    );
+    const newPages = paginateByHeight(book.chapters, measureRef.current);
+    setPages(newPages);
+  }, [book]);
 
   return (
     <div className="relative">
-      {/* Book */}
-      <div className="flex flex-row w-200 h-125 mx-auto rounded-2xl shadow-[0_20px_60px_rgba(0,0,0,0.6)] bg-amber-100 relative">
-        <div className="absolute inset-0 pointer-events-none rounded-2xl bg-[radial-gradient(circle_at_30%_30%,rgba(255,255,255,0.2),transparent_60%)] z-10" />
-          {/* Left Page */}
-          <div className="flex-1 p-8 overflow-hidden text-black pl-12 bg-linear-to-r from-black/10 to-transparent">
-            <h2 className="text-center text-2xl mb-4">{leftPage.title}</h2>
-            {/* whitespace-pre-line preserves the line breaks in the text, keeping the original formatting */}
-            <p className="text-justify whitespace-pre-line text-xs">
-              {leftPage.fullText}
-            </p>
-          </div>
-
-        {/* Right Page */}
-        <div className="flex-1 p-8 overflow-hidden text-black border-l border-black/20 pr-12 bg-linear-to-l from-black/10 to-transparent">
-          <h2 className="text-center text-2xl mb-4">{rightPage.title}</h2>
-          <p className="text-justify whitespace-pre-line text-xs">
-            {rightPage.fullText}
-          </p>
-        </div>
+      {/* Measure ref */}
+      <div
+        ref={measureRef}
+        className={`
+          absolute invisible pointer-events-none 
+          w-250 h-150 mx-auto
+          overflow-hidden
+          p-8 pb-12
+        `}
+      >
+              <h2 className="text-center text-2xl mb-4"></h2>
+              {/* whitespace-pre-line preserves the line breaks in the text, keeping the original formatting */}
+              <p className="w-full text-justify whitespace-pre-line text-xs"></p>
       </div>
-      {/* Controls */}
-      <div className="flex w-250 justify-between p-8 mt-4">
+      <div className="flex flex-row">
+        {/* Previous button */}
         <button
-          onClick={previousSpread}
-          disabled={spreadIndex === 0}
-          className="px-4 py-2 text-md text-black rounded-2xl shadow-md transistion bg-blue-600 hover:bg-blue-400 hover:shadow-xl"
-        >
-          Previous Page
-        </button>
+            onClick={previousSpread}
+            disabled={spreadIndex === 0}
+            className={`
+            mx-4 my-auto px-4 py-2 text-xs text-slate-50
+            max-h-fit rounded-2xl shadow-md 
+            transistion bg-blue-600 
+            hover:bg-blue-400 hover:shadow-xl
+          `}
+          >
+            Previous Page
+          </button>
+      {/* Book */}
+        <div className="flex flex-row w-250 h-150 mx-auto my-auto rounded-2xl shadow-[0_20px_60px_rgba(0,0,0,0.6)] bg-amber-100 relative">
+          <div className="flex h-full flex-row">
+            {/* Left Page */}
+            <div
+              className={`
+              flex-1 overflow-hidden text-black 
+              bg-linear-to-r from-black/10 to-transparent 
+            `}
+            >
+              <div className="p-8 pl-12 pb-12">
+                <h2 className="text-center text-2xl mb-4">{leftPage.title}</h2>
+                {/* whitespace-pre-line preserves the line breaks in the text, keeping the original formatting */}
+                <p className="w-full text-justify whitespace-pre-line text-xs">
+                  {leftPage.fullText}
+                </p>
+              </div>
+            </div>
+
+            {/* Right Page */}
+            <div
+              className={`
+              flex-1 overflow-hidden text-black border-l
+              bg-linear-to-l from-black/10 to-transparent 
+            `}
+            >
+              <div className="p-8 pr-12 pb-12">
+                <h2 className="text-center text-2xl mb-4">{rightPage.title}</h2>
+                <p className="w-full text-justify whitespace-pre-line text-xs">
+                  {rightPage.fullText}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            className={`
+          absolute inset-0 pointer-events-none rounded-2xl z-10
+          bg-[radial-gradient(circle_at_30%_30%,rgba(255,255,255,0.2),transparent_60%)]
+          `}
+          />
+        </div>
         <button
-          onClick={nextSpread}
-          disabled={spreadIndex === totalSpreads - 1}
-          className="px-4 py-2 text-md text-black rounded-2xl shadow-md transistion bg-blue-600 hover:bg-blue-400 hover:shadow-xl"
-        >
-          Next Page
-        </button>
+            onClick={nextSpread}
+            disabled={spreadIndex === totalSpreads - 1}
+            className={`
+            mx-4 my-auto px-4 py-2 text-xs text-slate-50
+            max-h-fit rounded-2xl shadow-md 
+            transistion bg-blue-600 
+            hover:bg-blue-400 hover:shadow-xl
+          `}
+          >
+            Next Page
+          </button>
       </div>
     </div>
   );
--- src/data/book.ts
+++ src/data/book.ts
@@ -44,11 +44,63 @@ The only way forward is through.
   `,
 };
 
+const chapter3: Chapter = {
+  id:"book1_chapter3",
+  title: "Akaash's Side",
+  fullText: `
+  Long, long ago, there was nothing in the universe, and God said let there be light. And a billion years later my parents decided to bring me into this world, the girl who decided to be a boy (iykyk)
+
+  So on and so forth, after wandering the earth aimlessly, I entered the gates of Christ (Deemed To Be University), Central Campus, Bangalore. The place I met the love of my life; I did mistake multiple side characters to be it, but life has a beautiful way of teaching lessons. This is the story of how I met THE love of my life.
+  
+  I was part of an organization called the Student Welfare Office in college, the media division of it. I was quite the social butterfly and I used to interact with all the new volunteers of the group. In that process, on 21 July 2023, I met Shraddha Shiggaon, my wife.
+
+  We started talking as we were working on a brochure of an event, and never looked back. I remember looking at her the first day and falling for her right there. I remember randomly having lunch together one day, laughing the whole time, getting to know each other. I remember showing what I wrote to her and her genuine appreciation and love for what I wrote. I remember both of us working on two different logos sitting together. I remember teaching her how a stylus works and the genuine joy I saw in her eyes as she discovered all that you can do with it. I remember bullying her, hitting her on the back of her head every time I saw her, scaring her with basketball, meeting her randomly on campus and in front of my class, running after her every time I spotted her. I remember when I sent her enough reels on buying me food that she actually ended up treating me lunch. I remember being invited on her birthday and covering her face with cake while her friends just laughed and took videos. I remember going to one of her favourite band’s concerts with her. And I remember being just a friend to her while all this was happening.
+
+  Yes, I never dated her throughout college. She was dating her ex; one messed up guy. I got to hear about how unhappy she was in the relationship, but I could never gather the courage to tell her she deserves more and I would want to give it to her, so I ended up being the one thing she needed – a genuine friend.
+
+  Madam Shraddha was a very difficult person to get a hold of, as she did not believe on speaking on calls or chatting, just meeting in person, if they get to meet. Later I get to know that madam was apparently very jealous whenever she saw me talk to other women. Madam was very confused about her mixed feeling about feeling this way, so she just tried to avoid me at all costs and only spoke to me on rare occasions, whenever she felt like it.
+
+  I do not get to pull her leg so much too, because in the duration of not being able to date her, I dated two women – one ended mildly, in a mature and decent manner, and the other was a landslide. It was a nuclear attack, which I quite honestly walked in willingly. Somehow, I came out with minor injuries, and home base had amazing healing capabilities, so I came out fine and had been thriving ever since.
+
+  I had graduated by then and after a lot of friction about leaving Bangalore, a place I started to recognise as “my” home, I went back to Thanjavur, to “my home”. I made an effort to stay in touch with a few of my good friends and spoke to them about how life is going. I worked in my father’s company, understanding what happens there and trying to get used to coming back home. 
+
+  Everything was amazing, I got comfortable with coming back home, I started an internship at an MNC, I got involved in my father’s business, got a new bike, laptop, a drone piloting license, a vacation – basically anything I wanted, but there was something missing.
+
+  For the longest time, I remember just one thought running in my head. It echoed throughout the day, and especially the moment I woke up, and the moment I slept, “I want my wife”. Surprisingly I was very clear about what I wanted in life. I remember sitting with my best friend and writing down 40 criteria’s, defining what my future partner should be like (obviously I wasn’t allowed to do it on my own as my previous choice was as bad as it could get). All of this, and still no one. During the day, it used to be fine, but in the early mornings and the silent nights, these voices got really loud.
+
+  Cut to 22 June 2025, Shraddha broke up with her ex. Fast forward to 24 June 2025, I get a text about it and I call her up immediately. Weirdly, she didn’t sound heartbroken. It felt as if she felt she should be heartbroken, but she only felt sad (little bit). I was out with family, so I had to cut the call short, unaware that one day she would end up being my family.
+
+  28 June 2025, Shraddha drunk calls me. She is drinking with her best friend Sangita, and Shraddha wants to have some fun in life. She wanted to do something stupid, so she wanted to call up her ex (another ex, not the same one). Somehow, I was the one who was contacted and I sat with her for 4 hours on call. This was a first. A person who said she doesn’t speak on call or text, spoke to me for 4 hours. 
+
+  For the most part of the call, she kept repeating she wanted to do something stupid, and after hour 3 I broke and told her to do me, coz I am stupid. Madam had different plans though, because I was the holy cow who could only be praised and worshipped, and nothing stupid should be performed with thy holiness. Between all this chaos, there was something that caught my ears though. Her friend Sangita had mentioned how she used to ship me and her together; I was almost on cloud 9. I could not believe my ears. And I was very disappointed when I was God-zoned by Shraddha. How appropriate I must say.
+
+  On 3 July 2025, Shraddha has the magic serum again and rings me up. We speak more and more. I have no memory of what was spoken, but I know I enjoyed every moment of it. Also, I was very annoyed by the fact that I was still God-zoned. By that point I had come to the conclusion that amongst the Kings of Friendzone, I was promoted to the God of Friendzone. I still tried to steer the conversation, trying drive out of the God-zone, but my efforts were in vain (so I thought).
+
+  On a side note, ever since the first drunk call on 28 June 2025, Shraddha had been chatting regularly for hours together, which was a premium feature that even her best friends or even her ex did not have access to; I do be one lucky man.
+
+  6 July 2025, surprisingly I am speaking to sober Shraddha. Madam wanted to drunk, but was in a mood to listen to me, so when I asked her not to drink, she didn’t. As the conversation got dragged out, Shraddha said that she felt like she was catching feelings. 
+
+  To say the least, I lost my shit.
+ 
+  This was all that I ever wanted. This was all that I had been thinking about and all that I ever desired. I had to be mature though, I had to check my 40 criteria’s and make sure that I was not a rebound. 
+
+  I told her about the 40 criteria, and we went through them together, and she fulfilled all of them. I do not know about the 36 guna’s, but all my 40 guna’s matched. It was like I manifested all that I wrote into a human form, and her having a face I would love to wake up to is the cherry on top (present tense because I still believe that, will believe it until death does us apart).
+
+  Post a lot of open-hearted conversation, I was forced to open myself up too. Now one might think, like any person who shares mutual feelings, I would’ve confessed right there. You are wrong. I beat around the bush for an hour, and then finally confessed. The reply was a sigh of relief, which given the situation was perfect.
+
+  On 7 July 2025, madam exhibited her expertise in the language of affection, and had me going speechless at multiple moments (for some context, it is really difficult to catch me go speechless). I was still unsure about the rebound; something had to be done. 
+
+  I initiated a probation period from 8 July 2025 to 8 August 2025; till the time she understands herself and decides what I am to her. Given how much I wanted her, each day felt like a year. And soon enough, the probation was ditched.
+
+  On 12 July 2025, I and THE love of my life, decided that we are stuck with each other, through all phases of life. Until death do us apart.
+`
+}
+
 export const book1 : Book = {
     id: "book1",
     title: "Anthology",
     author: "J Akaash",
     blurb: "This book is a compilation of all the stories that Akaash has written over the years.",
     aboutAuthor: "Akaash is a young writer who is trying to find his voice while he is learning tech.",
-    chapters: [chapter1, chapter2]
+    chapters: [chapter1, chapter2, chapter3]
 }
